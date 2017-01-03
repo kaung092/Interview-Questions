29c700184ff56af218223407a81da65e42b38dc5
Kaung Htet <kaung.092@gmail.com>
2017-01-03 14:39:32 -0500

    Update NumToTextConverter.php

--- 5/NumToTextConverter.php
+++ 5/NumToTextConverter.php
@@ -1,31 +1,31 @@
 <?php         
 //This file is hostsed on different server. (www.kaunghtet.net/temp/NumToTextConverter.php)
 
-  header("Access-Control-Allow-Origin: *");
-  header('content-type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header('content-type: application/json; charset=utf-8');
 
-	include $_SERVER['DOCUMENT_ROOT']."/temp/DAO.php";
+include $_SERVER['DOCUMENT_ROOT']."/temp/DAO.php";
 
-	$num = $_POST["userInput"];
+$num = $_POST["userInput"];
 
-  $result=null;                                                                                       
-  if($num%3==0  && $num%5==0){
-    $result = "Bisnow Meida";
-  }           
-  else if($num%3==0){
-    $result = "Bisnow";
-  }           
-  else if($num%5==0){
-    $result = "Media";
-  }           
+$result=null;                                                                                       
+if($num%3==0  && $num%5==0){
+	$result = "Bisnow Meida";
+}           
+else if($num%3==0){
+	$result = "Bisnow";
+}           
+else if($num%5==0){
+	$result = "Media";
+}           
 
 // - Save the user’s input to a MySQL tracking table
-	ob_start();
-	$dao = new MyDAO();
-	$dao->logInput($num);
-	ob_end_clean();
+ob_start();
+$dao = new MyDAO();
+$dao->logInput($num);
+ob_end_clean();
 
-  echo json_encode($result);
+echo json_encode($result);
 
 
 ?>
